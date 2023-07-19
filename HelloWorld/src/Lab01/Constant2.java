4b4f36876b0addc378e2f86007b48ce2bca1ce71
Hungnguyen1105 <hbday2k3@gmail.com>
2023-07-19 22:51:18 +0700

    Canstant 2

---
+++ HelloWorld/src/Lab01/Constant2.java
@@ -0,0 +1,11 @@
+package Lab01;
+
+public class Constant2 {
+    public static final double CM_PER_INCH = 2.54;
+
+    public static void main(String[] args) {
+        double paperWitdh = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters: " + paperWitdh * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+    }
+}
