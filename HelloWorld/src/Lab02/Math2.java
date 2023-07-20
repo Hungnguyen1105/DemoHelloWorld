136d9cc0076497d4ef6207ed11e8c83c736daeea
Hungnguyen1105 <hbday2k3@gmail.com>
2023-07-20 21:37:13 +0700

    Update Math2.java

--- HelloWorld/src/Lab02/Math2.java
+++ HelloWorld/src/Lab02/Math2.java
@@ -4,7 +4,9 @@ public class Math2 {
     public static void main(String[] args) {
         double x = 9.997;
         int nx = (int)Math.round(x);
-        // cau lenh Math,Round dung de lam tron so 
+        // cau lenh Math,Round dung de lam tron so
         System.out.println(nx);
+        // in ra nx 
+
     }
 }
