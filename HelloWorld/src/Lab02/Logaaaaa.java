13bf98d1a846d633298cd28cdc6b15a4163b8e89
Hungnguyen1105 <hbday2k3@gmail.com>
2023-07-20 16:31:35 +0700

    Logaaaa

---
+++ HelloWorld/src/Lab02/Logaaaaa.java
@@ -0,0 +1,23 @@
+package Lab02;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.sqrt;
+
+public class Logaaaaa {
+    public static void main(String[] args) {
+        System.out.println("The square root of \u03C0 is " + sqrt(PI));
+        // in ra man hinh can bac hai sqrt can bac 2 cua pi 3.14.....
+        // u03C0" được sử dụng để biểu thị ký tự pi, đại diện cho giá trị xấp xỉ của π, tức là 3.141592653589793.
+        int n =123456789;
+        // gan gia n int bang 123456789
+        float f= n;
+        System.out.println(f);
+
+        double x = 9.997;
+        // so o dang double
+        int nx = (int)x;
+        // chuyen doi so tu double sang interger
+        System.out.println(nx);
+        // in ra man hinh
+    }
+}
--- HelloWorld/src/Lab02/Mathhh.java
+++ HelloWorld/src/Lab02/Mathhh.java
@@ -5,7 +5,7 @@ public class Mathhh {
         double x = 16;
         // khai bao gan x = 16
         double y = Math.sqrt(x);
-        // khai bao y va lay can bac 2 cua x
+        // khai bao y va lay can bac 2 cua x va math.sprt lay can bac hai cua bien x da gan
         System.out.println(y);
         // in ra y
     }
