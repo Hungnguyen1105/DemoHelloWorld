e94cbca87d9abfc39abdac252bfb9ea84b576b9f
Hungnguyen1105 <hbday2k3@gmail.com>
2023-07-20 16:47:46 +0700

    Create Math2.java

---
+++ HelloWorld/src/Lab02/Math2.java
@@ -0,0 +1,10 @@
+package Lab02;
+
+public class Math2 {
+    public static void main(String[] args) {
+        double x = 9.997;
+        int nx = (int)Math.round(x);
+        // cau lenh Math,Round dung de lam tron so 
+        System.out.println(nx);
+    }
+}
