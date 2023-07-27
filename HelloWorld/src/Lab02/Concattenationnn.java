ec8d900a01b89b357416c097dc94f96169e13897
Hungnguyen1105 <hbday2k3@gmail.com>
2023-07-27 13:48:48 +0700

    substring and concattenation

---
+++ HelloWorld/src/Lab02/Concattenationnn.java
@@ -0,0 +1,11 @@
+package Lab02;
+
+public class Concattenationnn {
+    public static void main(String[] args) {
+        String expletive = "Expletive";
+        String PG13 = "deleted";
+        // khai bao gan gia tri
+        System.out.println("The answer is" + expletive + PG13 );
+        // noi chuoi thuc hien o dung lenh in
+    }
+}
---
+++ HelloWorld/src/Lab02/SubStringgg.java
@@ -0,0 +1,12 @@
+package Lab02;
+
+public class SubStringgg {
+    public static void main(String[] args) {
+        String greeting = "Nguyen Duy Hung";
+        // cho 1 chuoi ki tu
+        String s = greeting.substring(0, 9);
+        // tim ra ki tu de hien thi tren man hinh
+        System.out.println(s);
+    }
+
+}
