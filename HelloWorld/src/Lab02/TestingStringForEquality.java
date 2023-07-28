d35e2f5fae140649d3bd7488e3bb6ff1159fe7fe
Hungnguyen1105 <hbday2k3@gmail.com>
2023-07-28 13:13:08 +0700

    Update TestingStringForEquality.java

--- HelloWorld/src/Lab02/TestingStringForEquality.java
+++ HelloWorld/src/Lab02/TestingStringForEquality.java
@@ -22,6 +22,7 @@ public class TestingStringForEquality {
         // gan 2 gia tri
         System.out.println("str4 equals str5 " + str4.equalsIgnoreCase(str5));
         // check str4 co bang str5 hay khong phunog thuc equalsIgnoreCase dung de so sánh tính đồng nhất giữa hai chuỗi mà không phân biệt hoa thường
+        // va day la 2 su khac biet vi equals and equalsIgnoreCase
 
 
 
